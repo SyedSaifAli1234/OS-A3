f77a29a2d8e880c051256ea76b8f65dfe7c81d63
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-04-15 22:28:56 +0500

    Update A3-2.c

--- A3-2.c
+++ A3-2.c
@@ -8,8 +8,8 @@
 
 char* buffer;
 char* buffer2;
-sem_t* cp;
-sem_t* fp;
+sem_t* buff1;
+sem_t* buff2;
 
 
 // void* CoronaPatients(void* param){
@@ -72,7 +72,8 @@ int main(){
 		perror ("Shared memory att not done\n");
 		exit(0);
 	}
-	*buffer = NULL;
+	printf("1\n"); 
+
 
 
 
@@ -86,26 +87,131 @@ int main(){
 		perror ("Shared memory att not done\n");
 		exit(0);
 	}
-	*buffer2 = NULL;
+	printf("2\n");
+
+
+
+
+	int shmid_buff1_sem = shmget(141414, 1024, 0644|IPC_CREAT); 								//Semaphore for Buff1 shared memory
+	if (shmid_buff1_sem < 0){
+		perror ("Shared memory for buff1_sem not created\n");
+		exit(0);
+	}
+	buff1 = (sem_t*) shmat(shmid_buff1_sem, NULL, 0);								
+
+	if (buff1 < 0){
+		perror ("Shared memory not attached\n");
+		exit(0);
+	}
+	sem_open("sem_buff1", O_CREAT|O_EXCL, 0644, 1);
+	int result = sem_unlink("sem_buff1");
+	if (result < 0){
+		perror("Error in unlinking semaphore buff1_sem\n");
+		exit(0);
+	}	
+	printf("3\n");
+
+
+
+
+
+	int shmid_buff2_sem = shmget(151515, 1024, 0644|IPC_CREAT); 								//Semaphore for Buff2 shared memory
+	if (shmid_buff2_sem < 0){
+		perror ("Shared memory for buff2_sem not created\n");
+		exit(0);
+	}
+	buff2 = (sem_t*) shmat(shmid_buff2_sem, NULL, 0);								
+
+	if (buff2 < 0){
+		perror ("Shared memory not attached\n");
+		exit(0);
+	}
+	sem_open("sem_buff2", O_CREAT|O_EXCL, 0644, 1);
+	result = sem_unlink("sem_buff2");
+	if (result < 0){
+		perror("Error in unlinking semaphore buff2_sem\n");
+		exit(0);
+	}
+	printf("4\n");
+
+
+
+
+
+
+
+
+
+
+	if(fork() == 0){																	//Child1
+		//A say read and enter in buffer1
+	}	
+	else{																				//Parent
+		if(fork() == 0){																//Child2
+			//B say read and enter in buffer1
+		}
+		else{																			//Parent
+			if(fork() == 0){															//Child3
+				//Buffer 1 se C reads and enter in Buffer 2
+			}
+			else{																		//Parent
+				if(fork() == 0){														//Child4
+					//D reads from Buffer 2 and prints
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	result = shmdt(buff1);																		//Detaching and Destroying here
+	if (result < 0){
+		perror("Error in removing semaphore buff1_sem\n");
+		exit(0);
+	}
+	result = shmdt(buff2);
+	if (result < 0){
+		perror("Error in removing semaphore buff2_sem\n");
+		exit(0);
+	}
+	result = shmdt(buffer);
+	if (result < 0){
+		perror("Error in removing semaphore buff2_sem\n");
+		exit(0);
+	}
+	result = shmdt(buffer2);
+	if (result < 0){
+		perror("Error in removing semaphore buff2_sem\n");
+		exit(0);
+	}
 
 	
-
-
-
-	// result = sem_unlink("sem_pcp");																//Detaching and Deleting
-	// if ( result < 0){
-	// 	perror("Error in removing semaphore sem_pcp\n");
-	// 	exit(0);
-	// }
-
-	// result = shmdt(pcp);
-	// if ( result < 0){
-	// 	perror("Error in removing semaphore sem_pcp\n");
-	// 	exit(0);
-	//}
 	shmctl(shmid_buff1, IPC_RMID, 0);
 	shmctl(shmid_buff2, IPC_RMID, 0);
-    //sem_destroy(pcp_sem);
+	shmctl(shmid_buff1_sem, IPC_RMID, 0);
+	shmctl(shmid_buff2_sem, IPC_RMID, 0);
+    sem_destroy(buff1);
+    sem_destroy(buff2);
 
 	printf("Works\n");
 	return 0;
